707525032dac4ba08d8eb61ee2d31ded2b674cd4
DavidFrontendDev <daviddevbackend@gmail.com>
2025-03-03 20:03:42 +0100

    Form a punto de empezar useReducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { act, useState } from "react";
 import { categories } from "../data/categories";
+import { Activity } from "../types";
 
 function Form() {
-  const [activity, setActivity] = useState({
+  const [activity, setActivity] = useState<Activity>({
     category: 1,
     name: "",
     calories: 0,
@@ -13,14 +14,25 @@ function Form() {
       | React.ChangeEvent<HTMLSelectElement>
       | React.ChangeEvent<HTMLInputElement>
   ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
     setActivity({
       ...activity,
-      [e.target.id]: e.target.value,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
+  };
+  const isValidActivity = () => {
+    const { calories, name } = activity;
+    return name.trim() !== "" && calories > 0;
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
   };
 
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category">Categoria:</label>
         <select
@@ -60,8 +72,9 @@ function Form() {
       </div>
       <input
         type="submit"
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-        value="Guardar Comida o Guardar Ejercicio"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-15"
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+        disabled={!isValidActivity()}
       />
     </form>
   );
